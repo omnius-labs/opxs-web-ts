989c8ede2ce805d0a0e45583c4b0ee09c10d0d6a
Lyrise <lyrise1984@gmail.com>
2023-06-14 14:13:12 +0900

    fix

--- app/health/page.tsx
+++ app/health/page.tsx
@@ -1,22 +1,46 @@
 'use client';
 
-export default async function page() {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_ORIGIN + '/api/v1/health');
-  const data = await response.json();
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface HealthResult {
+  mode: string;
+  git_sha: string;
+  git_semver: string;
+}
+
+const DefaultHealthResult: HealthResult = {
+  mode: '',
+  git_sha: '',
+  git_semver: ''
+};
+
+export default function page() {
+  const [state, setState] = useState<HealthResult>(DefaultHealthResult);
+
+  useEffect(() => {
+    axios.get(process.env.NEXT_PUBLIC_API_ORIGIN + '/api/v1/health').then((res) => {
+      setState(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <table>
-      <tr>
-        <td>mode</td>
-        <td>{data.mode}</td>
-      </tr>
-      <tr>
-        <td>git_sha</td>
-        <td>{data.git_sha}</td>
-      </tr>
-      <tr>
-        <td>git_semver</td>
-        <td>{data.git_semver}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>mode</td>
+          <td>{state.mode}</td>
+        </tr>
+        <tr>
+          <td>git_sha</td>
+          <td>{state.git_sha}</td>
+        </tr>
+        <tr>
+          <td>git_semver</td>
+          <td>{state.git_semver}</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
