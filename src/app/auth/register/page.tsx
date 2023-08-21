6b71aeef7bede72383404680463615a6188cba73
Lyrise <lyrise1984@gmail.com>
2023-08-22 03:14:23 +0900

    fix

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -48,8 +48,10 @@ export default function Page() {
       <div className="w-96">
         <div className="flex-col flex justify-center items-center">
           <h1 className="text-white mb-6 text-4xl font-bold">Register</h1>
-          <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <button onClick={handleSignUpWithGoogle}>Sign up with Google</button>
+          <div className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5">
+            <button className="w-full" onClick={handleSignUpWithGoogle}>
+              Sign up with Google
+            </button>
           </div>
         </div>
 
@@ -65,46 +67,48 @@ export default function Page() {
           </div>
           <form onSubmit={handleSignUpWithEmail}>
             <div className="mb-6">
-              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-white">
                 Name
               </label>
               <input
                 type="name"
                 id="name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder=""
                 onChange={(e) => setName(e.target.value)}
                 required
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
                 Email address
               </label>
               <input
                 type="email"
                 id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder=""
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">
                 Password
               </label>
               <input
                 type="password"
                 id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
             </div>
 
-            <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              <button type="submit">Submit</button>
+            <div className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5">
+              <button className="w-full" type="submit">
+                Submit
+              </button>
             </div>
           </form>
         </div>
--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -25,8 +25,8 @@ export default function Page() {
     <main className="h-screen w-screen flex justify-center items-center bg-gray-900">
       <div className="p-16 h-full w-full">
         <h1 className="text-white mb-6 text-3xl font-bold">Health</h1>
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-gray-400">
+          <thead className="text-xs uppercase bg-gray-700">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Key
@@ -38,8 +38,8 @@ export default function Page() {
           </thead>
           <tbody>
             {state.map((item, index) => (
-              <tr key={item.key} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <tr key={item.key} className="bg-white border-b bg-gray-800 border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-white">
                   {item.key}
                 </th>
                 <td className="px-6 py-4">{item.value}</td>
