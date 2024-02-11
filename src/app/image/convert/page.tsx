a22fe49da9c9495003a03cd2e20db590a9d89f1c
Lyrise <lyrise1984@gmail.com>
2024-02-11 14:29:13 +0900

    fix

--- src/app/image/convert/page.tsx
+++ src/app/image/convert/page.tsx
@@ -49,9 +49,28 @@ export default function Page() {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag n drop some files here, or click to select files</p>
+    <div className="relative flex flex-col p-4 text-gray-400 border border-gray-200 rounded" {...getRootProps()}>
+      <input
+        className="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+        {...getInputProps()}
+      />
+      <div className="flex flex-col items-center justify-center py-10 text-center">
+        <svg
+          className="w-6 h-6 mr-1 text-current-50"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>
+        <p className="m-0">Drag your files here or click in this area.</p>
+      </div>
     </div>
   );
 }
