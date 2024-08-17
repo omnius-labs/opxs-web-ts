fcb190cc436a2119cf65a1fce68b066b870dae3d
Lyrise <lyrise1984@gmail.com>
2024-08-18 08:37:52 +0900

    fix

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react';
 import Home from '../src/app/page';
 
+jest.mock('next/navigation', () => ({
+  useRouter: () => jest.fn()
+}));
+
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
